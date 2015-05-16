9c5fb4f161055e54865eace3fc44c78cf2099225
Ben Newman <ben@meteor.com>
2015-05-16 19:40:51 -0400

    Update Babel to v5.4.3.
    There have been a lot of speed improvements lately!

--- packages/babel/package.js
+++ packages/babel/package.js
@@ -2,11 +2,11 @@ Package.describe({
   summary: "Parser/transpiler for ECMAScript 6+ syntax",
   // Tracks the npm version below.  Use wrap numbers to increment
   // without incrementing the npm version.
-  version: '5.1.11'
+  version: '5.4.3'
 });
 
 Npm.depends({
-  'babel-core': '5.1.11'
+  'babel-core': '5.4.3'
 });
 
 Package.onUse(function (api) {
