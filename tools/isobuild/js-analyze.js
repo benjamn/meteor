f7b9f3fb2826d648bde2e7bc90b528b21299595f
Ben Newman <ben@meteor.com>
2015-10-06 15:30:32 -0400

    Implement findImportedModuleIdentifiers.

--- tools/isobuild/js-analyze.js
+++ tools/isobuild/js-analyze.js
@@ -13,6 +13,112 @@ function tryToParse(source) {
   }
 }
 
+var dependencyKeywordPattern = /\b(require|import|export)\b/g;
+
+export function findImportedModuleIdentifiers(source) {
+  const identifiers = {};
+  const possibleIndexes = [];
+  let match;
+
+  dependencyKeywordPattern.lastIndex = 0;
+  while ((match = dependencyKeywordPattern.exec(source))) {
+    possibleIndexes.push(match.index);
+  }
+
+  if (!possibleIndexes.length) {
+    return [];
+  }
+
+  const ast = tryToParse(source);
+
+  function walk(node, left, right) {
+    if (left >= right) {
+      // The window of possible indexes is empty, so we can ignore
+      // the entire subtree rooted at this node.
+    } else if (Array.isArray(node)) {
+      for (var i = 0, len = node.length; i < len; ++i) {
+        walk(node[i], left, right);
+      }
+    } else if (isNode(node)) {
+      const start = node.start;
+      const end = node.end;
+
+      // Narrow the left-right window to exclude possible indexes
+      // that fall outside of the current node.
+      while (left < right && possibleIndexes[left] < start) ++left;
+      while (left < right && end < possibleIndexes[right - 1]) --right;
+
+      if (left < right) {
+        let id = getRequiredModuleId(node);
+        if (typeof id === "string") {
+          identifiers[id] = node;
+          return;
+        }
+
+        id = getImportedModuleId(node);
+        if (typeof id === "string") {
+          identifiers[id] = node;
+          return;
+        }
+
+        // Continue traversing the children of this node.
+        for (const key of Object.keys(node)) {
+          switch (key) {
+          case "type":
+          case "loc":
+          case "start":
+          case "end":
+            // Ignore common keys that are never nodes.
+            continue;
+          }
+
+          walk(node[key], left, right);
+        }
+      }
+    }
+  }
+
+  walk(ast, 0, possibleIndexes.length);
+
+  return identifiers;
+}
+
+function isNode(value) {
+  return value
+    && typeof value === "object"
+    && typeof value.type === "string"
+    && typeof value.start === "number"
+    && typeof value.end === "number";
+}
+
+function getRequiredModuleId(node) {
+  if (node.type === "CallExpression" &&
+      node.callee.type === "Identifier" &&
+      node.callee.name === "require") {
+    const args = node.arguments;
+    const argc = args.length;
+    if (argc > 0) {
+      const arg = args[0];
+      if (arg.type === "Literal" &&
+          typeof arg.value === "string") {
+        return arg.value;
+      }
+    }
+  }
+}
+
+function getImportedModuleId(node) {
+  if (node.type === "ImportDeclaration" ||
+      node.type === "ExportAllDeclaration" ||
+      node.type === "ExportNamedDeclaration") {
+    // The .source of an ImportDeclaration or Export{Named,All}Declaration
+    // is always a string-valued Literal node, if not null.
+    if (isNode(node.source)) {
+      return node.source.value;
+    }
+  }
+}
+
 // Analyze the JavaScript source code `source` and return a dictionary of all
 // globals which are assigned to in the package. The values in the dictionary
 // are all `true`.
